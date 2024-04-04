832a70ad11a1809b3eb3dcbf1d9fbd7c2b790a04
Liam Bard <liam.axel.bard@gmail.com>
2024-04-04 14:46:31 +0200

    bilder och menu items

---
+++ src/data/menuItems.js
@@ -0,0 +1,36 @@
+import Köttbullar from "../assets/Köttbullar.png";
+const menuItems = [
+    {
+      name: "Köttbullar med Potatismos",
+      image: Köttbullar, 
+      price: 150,
+      description: "Svenska köttbullar serverade med potatismos, brunsås, lingon och pressgurka."
+    },
+    {
+      name: "Korvstroganoff",
+      price: 120,
+      description: "En klassisk rätt gjord på strimlad korv i en stroganoff-sås, serverad med ris."
+    },
+    {
+      name: "Panerad fisk med koktpotatis",
+      price: 150,
+      description: "Panerad vit fisk som serveras med kokt potatis och remouladsås."
+    },
+    {
+      name: "Pyttipanna",
+      price: 120,
+      description: "Traditionell pyttipanna med tärnad potatis, korv och lök, toppad med stekt ägg och rödbetor."
+    },
+    {
+      name: "Pannbiffar",
+      price: 150,
+      description: "Hemlagade pannbiffar med löksky, serverade med potatispuré och lingon."
+    },
+    {
+      name: "Biff Wallenberg",
+      price: 150,
+      description: "Klassisk Biff Wallenberg serverad med potatispuré, ärtor, lingon och skirat smör."
+    }
+  ];
+  
+    export default menuItems;
--- src/routes/LandingPage.jsx
+++ src/routes/LandingPage.jsx
@@ -1,8 +1,25 @@
+import menuItems from "../data/menuItems";
+
+
 const LandingPage = () => {
     return (
+        <>
         <div>
         <h1>Landing Page</h1>
+                <menuItems />
         </div>
+        
+        <div>
+      {menuItems.map((item, index) => (
+        <div key={index}>
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+          <p>Pris: {item.price} kr</p>
+          <img src={item.image} alt={item.name} />
+        </div>
+      ))}
+    </div>
+        </>
     );
 }
 
