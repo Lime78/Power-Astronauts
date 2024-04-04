0e0fc0a0ceedbb0d31118d81189b4b802de82950
Liam Bard <liam.axel.bard@gmail.com>
2024-04-04 13:17:20 +0200

    hejsan

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routers/router.jsx'
+import { router } from './routes/router.jsx'
+import { LandingPage } from './routes/LandingPage.jsx'
 
 
 
